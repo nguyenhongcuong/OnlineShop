a0b869e8644bd9bf07150b6769e9b891f12144fe
S2WHYLOV3\s2whylov3 <nguyenhongcuong69@gmail.com>
2018-03-21 17:25:19 +0700

    Add entity framework code first for data

---
+++ OnlineShop.Data/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace OnlineShop.Data.Infrastructure
+{
+    public abstract class RepositoryBase<T> where T : class
+    {
+        #region Properties
+
+        private OnlineShopDbContext _dbContext;
+        private readonly IDbSet<T> _dbSet;
+
+        #endregion
+
+        protected IDbFactory DbFactory { get; private set; }
+
+        protected OnlineShopDbContext DbContext
+        {
+            get { return _dbContext ?? (_dbContext = DbFactory.Init()); }
+        }
+
+        protected RepositoryBase(IDbFactory dbFactory)
+        {
+            DbFactory = dbFactory;
+            _dbSet = DbContext.Set<T>();
+        }
+
+        #region Implementation
+
+        public virtual void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual void DeleteMulti(Expression<Func<T, bool>> where)
+        {
+            IEnumerable<T> objects = _dbSet.Where(where).AsEnumerable();
+            foreach (T obj in objects)
+            {
+                _dbSet.Remove(obj);
+            }
+        }
+
+        public virtual T GetSingleById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual IEnumerable<T> GetMany(Expression<Func<T, bool>> where, string includes)
+        {
+            return _dbSet.Where(where).ToList();
+        }
+
+        public virtual int CountdownEvent(Expression<Func<T, bool>> where)
+        {
+            return _dbSet.Count(where);
+        }
+
+        public IQueryable<T> GetAll(string[] includes = null)
+        {
+            //HANDLE INCLUDES FOR ASSOCIATED OBJECTS IF APPLICABLE
+            if (includes != null && includes.Any())
+            {
+                var query = _dbContext.Set<T>().Include(includes.First());
+                foreach (var include in includes.Skip(1))
+                    query = query.Include(include);
+                return query.AsQueryable();
+            }
+
+            return _dbContext.Set<T>().AsQueryable();
+        }
+
+        public T GetSingleByCondition(Expression<Func<T, bool>> expression, string[] includes = null)
+        {
+            return GetAll(includes).FirstOrDefault(expression);
+        }
+
+        public virtual IQueryable<T> GetMulti(Expression<Func<T, bool>> predicate, string[] includes = null)
+        {
+            //HANDLE INCLUDES FOR ASSOCIATED OBJECTS IF APPLICABLE
+            if (includes != null && includes.Count() > 0)
+            {
+                var query = _dbContext.Set<T>().Include(includes.First());
+                foreach (var include in includes.Skip(1))
+                    query = query.Include(include);
+                return query.Where<T>(predicate).AsQueryable<T>();
+            }
+
+            return _dbContext.Set<T>().Where<T>(predicate).AsQueryable<T>();
+        }
+
+        public virtual IQueryable<T> GetMultiPaging(Expression<Func<T, bool>> predicate, out int total, int index = 0, int size = 20, string[] includes = null)
+        {
+            int skipCount = index * size;
+            IQueryable<T> _resetSet;
+
+            //HANDLE INCLUDES FOR ASSOCIATED OBJECTS IF APPLICABLE
+            if (includes != null && includes.Any())
+            {
+                var query = _dbContext.Set<T>().Include(includes.First());
+                foreach (var include in includes.Skip(1))
+                    query = query.Include(include);
+                _resetSet = predicate != null ? query.Where<T>(predicate).AsQueryable() : query.AsQueryable();
+            }
+            else
+            {
+                _resetSet = predicate != null ? _dbContext.Set<T>().Where<T>(predicate).AsQueryable() : _dbContext.Set<T>().AsQueryable();
+            }
+
+            _resetSet = skipCount == 0 ? _resetSet.Take(size) : _resetSet.Skip(skipCount).Take(size);
+            total = _resetSet.Count();
+            return _resetSet.AsQueryable();
+        }
+
+        public bool CheckContains(Expression<Func<T, bool>> predicate)
+        {
+            return _dbContext.Set<T>().Count<T>(predicate) > 0;
+        }
+        #endregion
+    }
+}
---
+++ OnlineShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OnlineShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory _dbFactory;
+        private OnlineShopDbContext _dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this._dbFactory = dbFactory;
+        }
+
+        public OnlineShopDbContext DbContext
+        {
+            get { return _dbContext ?? (_dbContext = _dbFactory.Init()); }
+        }
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
