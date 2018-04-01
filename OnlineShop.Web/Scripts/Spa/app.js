602ab84f983dce49ddf1c3063d7a36e331245a11
S2WHYLOV3\s2whylov3 <nguyenhongcuong69@gmail.com>
2018-04-01 23:52:18 +0700

    scope and rootScope

---
+++ OnlineShop.Web/Scripts/Spa/app.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../Plugins/angular/angular.js" />
+
+
+var myApp = angular.module('myModule', []);
+
+myApp.controller("teacherController", teacherController);
+myApp.controller("studentController", studentController);
+myApp.controller("schoolController", schoolController);
+
+//myController.$inject = ['$scope'];
+
+// delare
+function teacherController($rootScope, $scope) {
+    //$scope.message = "This is my message form TeacherController";
+    //$rootScope.message = "This is my message form TeacherController";
+}
+
+function studentController($scope) {
+    //$scope.message = "This is my message form StudentController";
+}
+
+function schoolController($scope) {
+    $scope.message = "This is my message from School";
+}
