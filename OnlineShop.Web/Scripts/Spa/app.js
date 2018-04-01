439b391f378bfe019f6747f01aef4c22a79daa2c
S2WHYLOV3\s2whylov3 <nguyenhongcuong69@gmail.com>
2018-04-02 00:47:40 +0700

    Directive in angular

--- OnlineShop.Web/Scripts/Spa/app.js
+++ OnlineShop.Web/Scripts/Spa/app.js
@@ -4,27 +4,37 @@
 var myApp = angular.module('myModule', []);
 
 myApp.controller("schoolController", schoolController);
-myApp.service('validater', validater);
+myApp.service('validaterService', validaterService);
+myApp.directive('demoDirective', demoDirective);
 
-schoolController.$inject = ['$scope', 'validater'];
+
+
+schoolController.$inject = ['$scope', 'validaterService'];
 
 function schoolController($scope, validater) {
     //validater.checkNumber(12);
     //$scope.message = "This is my message from School";
-    $scope.checkNumber= function() {
+    $scope.checkNumber = function () {
         $scope.message = validater.check($scope.numb);
     }
-   
+
     $scope.numb = 1;
 }
 
 
-function validater($window) {
+function validaterService($window) {
     return {
         check: checkNumber
     }
     function checkNumber(input) {
         //input % 2 === 0 ? $window.alert('This is even') : $window.alert('This is odd');
         return input % 2 === 0 ? 'This is even' : 'This is odd';
+    }
+}
+
+function demoDirective() {
+    return {
+        templateUrl: '/Scripts/Spa/demoDirective.html'
+        //template: '<h2>This is demo custom directive'
     }
 }
