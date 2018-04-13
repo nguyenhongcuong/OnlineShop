cd1b19cdb5479c1af63c064fee1aadeacfc4e41c
S2WHYLOV3\s2whylov3 <nguyenhongcuong69@gmail.com>
2018-04-13 10:19:44 +0700

    Tạo form đăng nhập và điều hướng giữa các trang

---
+++ OnlineShop.Web/app/components/login/loginController.js
@@ -0,0 +1,13 @@
+﻿(function (app) {
+    app.controller('loginController', loginController);
+    loginController.$inject = ['$scope', '$state'];
+    function loginController($scope, $state) {
+        $scope.loginSubmit = loginSubmit;
+
+        function loginSubmit() {
+            $state.go('home');
+        }
+    }
+
+
+})(angular.module('onlineShop'));
