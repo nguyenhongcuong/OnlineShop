6d7db2e3c49da45aa3aa84051e6d71bb20b79dc1
S2WHYLOV3\s2whylov3 <nguyenhongcuong69@gmail.com>
2018-04-14 16:41:37 +0700

    Cấu hình RouterConfig tạo url thân thiện

--- OnlineShop.Web/App_Start/RouteConfig.cs
+++ OnlineShop.Web/App_Start/RouteConfig.cs
@@ -14,9 +14,38 @@ namespace OnlineShop.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "dang-nhap",
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
+                namespaces: new string[] { "OnlineShop.Web.Controllers" }
+            );
+
+            routes.MapRoute(
+                name: "About",
+                url: "gioi-thieu",
+                defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
+                namespaces:new string[] { "OnlineShop.Web.Controllers" }
+            );
+
+            routes.MapRoute(
+                name: "Product Category",
+                url: "{alias}/pc-{id}",
+                defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
+                namespaces: new string[] { "OnlineShop.Web.Controllers" }
+            );
+
+            routes.MapRoute(
+                name: "Product Detail",
+                url: "{alias}/p-{id}",
+                defaults: new { controller = "Product", action = "Detail", id = UrlParameter.Optional },
+                namespaces: new string[] { "OnlineShop.Web.Controllers" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "OnlineShop.Web.Controllers" }
             );
         }
     }
---
+++ OnlineShop.Web/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineShop.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
