fd9ac8a80f572c48f317e8e8f4d8b55a9202a6f0
S2WHYLOV3\s2whylov3 <nguyenhongcuong69@gmail.com>
2018-05-13 15:49:23 +0700

    Output cache

--- OnlineShop.Web/Controllers/HomeController.cs
+++ OnlineShop.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using AutoMapper;
 using OnlineShop.Model.Models;
 using OnlineShop.Service;
@@ -16,13 +17,15 @@ namespace OnlineShop.Web.Controllers
         private ICommonService _commonService;
         private IProductService _productService;
 
-        public HomeController(IProductCategoryService productCategoryService, ICommonService commonService, IProductService productService)
+        public HomeController(IProductCategoryService productCategoryService , ICommonService commonService , IProductService productService)
         {
             _productCategoryService = productCategoryService;
             _commonService = commonService;
             _productService = productService;
 
         }
+
+        [OutputCache(Duration = 120 , Location = OutputCacheLocation.Server)]
         public ActionResult Index()
         {
             var slides = _commonService.GetSlides();
@@ -31,34 +34,21 @@ namespace OnlineShop.Web.Controllers
 
 
 
-            var slideViewModels = Mapper.Map<IEnumerable<Slide>, IEnumerable<SlideViewModel>>(slides);
-            var lastProductViewModels = Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(lastProducts);
-            var topSaleProductViewModels = Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(topSaleProducts);
+            var slideViewModels = Mapper.Map<IEnumerable<Slide> , IEnumerable<SlideViewModel>>(slides);
+            var lastProductViewModels = Mapper.Map<IEnumerable<Product> , IEnumerable<ProductViewModel>>(lastProducts);
+            var topSaleProductViewModels = Mapper.Map<IEnumerable<Product> , IEnumerable<ProductViewModel>>(topSaleProducts);
 
             var homeViewModel = new HomeViewModel
             {
-                SlideViewModels = slideViewModels,
-                LastProductViewModels = lastProductViewModels,
+                SlideViewModels = slideViewModels ,
+                LastProductViewModels = lastProductViewModels ,
                 TopSaleProductViewModels = topSaleProductViewModels
             };
             return View(homeViewModel);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         [ChildActionOnly]
+        [OutputCache(Duration = 3600)]
         public ActionResult _FooterPartial()
         {
             return PartialView();
@@ -66,16 +56,18 @@ namespace OnlineShop.Web.Controllers
 
 
         [ChildActionOnly]
+        [OutputCache(Duration = 3600)]
         public ActionResult _HeaderPartial()
         {
             return PartialView();
         }
 
         [ChildActionOnly]
+        [OutputCache(Duration = 3600)]
         public ActionResult _CategoriesPartial()
         {
             var productCategories = _productCategoryService.GetAll();
-            var productCategoryViewModels = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(productCategories);
+            var productCategoryViewModels = Mapper.Map<IEnumerable<ProductCategory> , IEnumerable<ProductCategoryViewModel>>(productCategories);
             return PartialView(productCategoryViewModels);
         }
     }
